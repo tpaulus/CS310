a7c29e1b77bfd30c34da1d2b01b157922f97813f
Tom Paulus <tom@tompaulus.com>
2017-02-04 16:55:39 -0800

    Updated Tester Class

--- src/prog1/P1Tester.java
+++ src/prog1/P1Tester.java
@@ -8,17 +8,38 @@ import data_structures.LinearListADT;
  *         Created on 1/23/17.
  */
 public class P1Tester {
-    private static LinearListADT<String> list = new ArrayLinearList<>();
-
     public static void main(String[] args) {
-        // Add
-        list.addLast("World");
-        list.addFirst("Hello ");
-        System.out.println(list.size());
-        list.remove("Hello ");
-        System.out.println(list.size());
+        LinearListADT<Integer> list = new ArrayLinearList<Integer>();
+        list.insert(1, 1);
+        list.insert(2, 2);
+        list.insert(3, 2);
+        list.insert(4, 2);
+        list.insert(5, 5);
+        try {
+            list.insert(2, 0);  // should throw an exception!
+        } catch (RuntimeException e) {
+            System.out.println("Exception Thrown - This is Good!");
+        }
+        try {
+            list.insert(77777, 7);  // should throw an exception!
+        } catch (RuntimeException e) {
+            System.out.println("Exception Thrown - This is Good!");
+        }
+        list.addFirst(-1);
+        list.addLast(-1);
+        //Should print -1,1,4,3,2,5,-1
+        for (int i : list)
+            System.out.println(i);
+
+        list.clear();
+        for (int i : list) // should not print anything, nor crash
+            System.out.println(i);
+
+        for (int i = 0; i < 100; i++)
+            list.insert((i + 1), 1);
+        for (int i : list) // should print 100 .. 1
+            System.out.print(i + " ");
+        System.out.println();
     }
 
-    // Possibly implement Unit Tests
-    // - Remove from Empty Array, etc.
 }
